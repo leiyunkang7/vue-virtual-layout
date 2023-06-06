b9d0ea50242027ab89f35ae767911cf9dc2ff52f
leiyunkang <leiyunkang7@outlook.com>
2023-06-06 22:34:34 +0800

    vue2.6初步运行

---
+++ packages/build-v2/rollup.config.ts
@@ -0,0 +1,60 @@
+
+import type { RollupOptions } from 'rollup';
+import { resolve } from 'node:path';
+import PluginDts from "rollup-plugin-dts";
+// import typescript from '@rollup/plugin-typescript';
+import { createVuePlugin } from 'vite-plugin-vue2'
+import path from 'path';
+// import PluginResolve from '@rollup/plugin-node-resolve';
+import esbuild from 'rollup-plugin-esbuild';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const input = resolve(__dirname, '../vite-project/src/components/vue-virtual-layout/index.ts')
+const externals = [
+  'vue-demi',
+  'vue',
+  '@vue-virtual-layout/v2',
+  '@vue/composition-api'
+]
+
+const config: RollupOptions[] = [
+  {
+    input,
+    output: [
+      {
+        file: resolve(__dirname, './dist/vue-virtual-layout.cjs'),
+        format: 'cjs',
+      },
+      {
+        file: resolve(__dirname, './dist/vue-virtual-layout.mjs'),
+        format: 'es',
+      },
+    ],
+    external: [...externals],
+    plugins: [
+      createVuePlugin({
+        jsx: true,
+        jsxOptions: {
+          compositionAPI: true
+        }
+      }),
+      esbuild(),
+      // typescript(),
+      // PluginResolve({
+      //   extensions: ['.js', '.jsx', '.ts', '.tsx']
+      // }),
+    ],
+  },
+  {
+    input: resolve(__dirname, './dist/vue-virtual-layout.mjs'),
+    output: [{ file: resolve(__dirname, './dist/vue-virtual-layout.d.ts'), format: 'es' }],
+    external: [...externals],
+    plugins: [
+        PluginDts({
+        // tsconfig: resolve(__dirname, "./tsconfig.json"),
+      })
+    ],
+  },
+];
+
+export default config;
---
+++ playgrounds/vue2/App.vue
@@ -0,0 +1,16 @@
+<script  lang="ts">
+import VueVirtualLayout from '@vue-virtual-layout/v2'
+
+export default {
+  components: {
+    VueVirtualLayout
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <VueVirtualLayout text="按钮"></VueVirtualLayout>
+  </div>
+</template>
